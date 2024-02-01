af949eac882cfd6e6bed5fde063229fae48a862f
YogeshV22 <yogeshverma@iiserb.ac.in>
2024-02-01 13:08:37 +0530

    step 14 added in job submission instruction

--- js/basic.js
+++
@@ -1,7 +0,0 @@
-function resizeIframe(obj) {
- try {
-     setTimeout(function(){
-         obj.style.height = (obj.contentWindow.document.body.scrollHeight) + 'px';
-         }, 300);
- } catch(e) {}
-}
--- js/slideshow.js
+++
@@ -1,5 +0,0 @@
-var caption_h = '50px';
-
-           jQuery(document).ready(function($) {
-            slideShow($,'slideshow-container-two');
-           });
